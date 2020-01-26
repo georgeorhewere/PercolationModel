d4cd8333bba8dcaa5546b6ff91eac79f36953229
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-01-26 19:57:24 +0100

    Initial percolation assignment checkin

---
+++ Percolation.java
@@ -0,0 +1,18 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+
+    private int[][] grid;
+
+    public Percolation(int n) {
+        grid = new int[n][n];
+
+    }
+
+}
